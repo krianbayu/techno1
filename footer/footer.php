546612cecc31f8a721cac2406e5a1febb8bb3bad
krianbayu <34035588+krianbayu@users.noreply.github.com>
2018-03-03 14:17:03 +0700

    Add files via upload

---
+++ footer/footer.php
@@ -0,0 +1,58 @@
+
+<!-- Footer
+================================================== -->
+<div id="footer" class="dark">
+	<!-- Main -->
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 col-sm-6">
+				Logo Fambus
+				<br><br>
+				<p></p>
+			</div>
+
+			<div class="col-md-4 col-sm-6 ">
+				<h4>Helpful Links</h4>
+				<ul class="footer-links">
+					<li><a href="#">My Account</a></li>
+					<li><a href="#">Google Drive</a></li>
+					<li><a href="#">My Business</a></li>
+				</ul>
+				<ul class="footer-links">
+          <li><a href="#">Fambus Policy</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Blog</a></li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
+
+			<div class="col-md-3  col-sm-12">
+				<h4>Contact Us</h4>
+				<div class="text-widget">
+					<span>Kuta Utara, Bali</span> <br>
+					Phone: <span>(+62) </span><br>
+					E-Mail:<span> <a href="#">krian@example.com</a> </span><br>
+				</div>
+
+				<ul class="social-icons margin-top-20">
+					<li><a class="facebook" href="#"><i class="icon-facebook"></i></a></li>
+					<li><a class="twitter" href="#"><i class="icon-twitter"></i></a></li>
+					<li><a class="gplus" href="#"><i class="icon-gplus"></i></a></li>
+					<li><a class="vimeo" href="#"><i class="icon-vimeo"></i></a></li>
+				</ul>
+
+			</div>
+
+		</div>
+
+		<!-- Copyright -->
+		<div class="row">
+			<div class="col-md-12">
+				<div class="copyrights"> Family Business - Universitas Ciputra.</div>
+			</div>
+		</div>
+
+	</div>
+
+</div>
+<!-- Footer / End -->
